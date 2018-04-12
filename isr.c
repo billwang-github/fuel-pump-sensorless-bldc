ba0616d0d0a0dc74ec3f036a406ad17dc0749b0b
billwang-github <38307221+billwang-github@users.noreply.github.com>
2018-04-12 19:56:42 +0800

    Add files via upload
    180412d
    -- 增加 hardware mask mode及hall sensor delay選項
    -- 參數化 PWM及hall noise filter

--- isr.c
+++ isr.c
@@ -1,210 +1,137 @@
+
 #include "HT66FM5440.h"
 #include "mydef.h"
 
-uint16 uiHallPeriod = 0, uiHallCnt = 0;
+uint16			uiHallPeriod = 0, uiHallCnt = 0;
 
-uint8 ucDragTmr = 0;
-uint16 uiDragDly = 0;
+uint8			ucDragTmr = 0;
+uint16			uiDragDly = 0;
 
-uint16 uiCommCycle = 0;
-uint8 ucCommStep = 0;
+uint16			uiCommCycle = 0;
+uint8			ucCommStep = 0;
 
-uint8 state_motor = STOP;
-uint8 ucCount1ms = 0;
-uint16 uiDutyRamp = 0;
+uint8			state_motor = STOP;
+uint8			ucCount1ms = 0;
+uint16			uiDutyRamp = 0;
 
-extern _bits status;
+extern _bits	status;
 
-void __attribute ((interrupt(0x28))) ISR_TM0(void) // For commutation period calculation, 16b
+
+void __attribute((interrupt(0x28))) ISR_TM0(void) // For commutation period calculation, 16b
 {
-	_int_pri10f = 0;
-	_tm0af = 0;	// clear TM1 interrup flag    
-	_pt0on = 0;					
-    WDT_RESET;
+	_int_pri10f 		= 0;
+	_tm0af				= 0;						// clear TM0 interrup flag	  
+	_pt0on				= 0;
+	WDT_RESET;
 }
 
-void __attribute ((interrupt(0x2c))) ISR_TM1(void) // For hall period calculation, 16b
+
+void __attribute((interrupt(0x2c))) ISR_TM1(void) // For hall period calculation, 16b
 {
-	_tm1af = 0;	// clear TM1 interrup flag    
-    _pt1on = 0; // disable TM1 
-    WDT_RESET;
+	_int_pri11f 		= 0;
+	_tm1af				= 0;						// clear TM1 interrup flag	  
+	_pt1on				= 0;						// disable TM1 
+	WDT_RESET;
 }
 
-void __attribute ((interrupt(0x38))) ISR_TM3(void)
+
+void __attribute((interrupt(0x38))) ISR_TM3(void) // 10b
 {
-	
-	//TO1 = 1;
-	CLRF_TM3;	// clear TM3 interrup flag    
-	 _pt3on = 0; // stop TM3
-    CLRF_HALL; // clear hall int flag
+	_int_pri14f 		= 0;
+	_tm3af 				= 0;
+	_pt3on				= 0;						// stop TM3
+
+	CLRF_TM3;										// clear TM3 interrup flag	  
+	CLRF_HALL;										// clear hall int flag
+
 	if ((uiCommCycle == 0) && (ucCommStep == 1))
-		INTEN_HALL = 0; // enable hall interrupt  
-	else
-    	INTEN_HALL = 1; // enable hall interrupt  
-    _integ0 |= 0x40; // CMP output  
-    WDT_RESET;
-    
+		INTEN_HALL = 0; // disable hall interrupt  
+	else 
+		INTEN_HALL = 1; // enable hall interrupt  
+
+	_integ0 			|= 0x40;					// CMP output  
+	WDT_RESET;
+
 }
+
 
 // 16MHz/16384 = 1.024ms
-void __attribute ((interrupt(0x3c))) ISR_TimeBase(void) 
+void __attribute((interrupt(0x3c)))
+ISR_TimeBase(void)
 {
 
 	CLRF_TMB;
-	
-	if (state_motor == DRAG)
-	{
-		if (ucDragTmr++ >= uiDragDly)
-		{			
-			Drag_Motor();										
-		}	
+
+	if (state_motor == DRAG) {
+		if (ucDragTmr++ >= uiDragDly) {
+			Drag_Motor();
+		}
 	}
-	
-	ucCount1ms ++;
-    if(ucCount1ms >= 1)	
-	{
-	    ucCount1ms = 0;
-	    bNmsFlag = 1;
+
+	ucCount1ms++;
+
+	if (ucCount1ms >= 1) {
+		ucCount1ms			= 0;
+		bNmsFlag			= 1;
 	}
-		
-    WDT_RESET;	
+
+	WDT_RESET;
 }
 
-void __attribute ((interrupt(0x04))) ISR_HALL(void)
+
+void __attribute((interrupt(0x04)))
+ISR_HALL(void)
 {
-	uint8  ucHallTimeTempL, ucHallTimeTempH;
+	uint8			ucHallTimeTempL, ucHallTimeTempH;
 
 	CLRF_HALL;
-	_pt1on = 0;
-    ucHallTimeTempH = _ptm1dh;    
-   	ucHallTimeTempL = _ptm1dl;
-    uiHallPeriod = (ucHallTimeTempH << 8) | ucHallTimeTempL;
-    RST_TM1;
+	_pt1on				= 0;
+	ucHallTimeTempH 	= _ptm1dh;
+	ucHallTimeTempL 	= _ptm1dl;
+	uiHallPeriod		= (ucHallTimeTempH << 8) | ucHallTimeTempL;
+	RST_TM1;
 
 	//TO1 = 0;	
 	if (uiHallCnt < 65534)
-		uiHallCnt++;    
-    
-    Commutation();
-    if (state_motor >= RAMP)
-    {
-    	TM3_Dly_Set(uiHallPeriod >> 3);
-    }
-    else
-    {
-    	TM3_Dly_Set(250);
-    	if (uiCommCycle == 2)
-    	{
-    		state_motor = RAMP;
-    	}    	
-    }
-    RST_TM3; 
-    
-    WDT_RESET;
-    
+		uiHallCnt++;
+
+	Commutation();
+
+	if (state_motor >= RAMP) {
+		TM3_Dly_Set(uiHallPeriod >> 2);
+	}
+	else {
+
+		if (uiCommCycle == 2) {
+			state_motor 		= RAMP;
+			_hchk_num			= NUM_RAMP;
+		}
+	}
+
+	RST_TM3;
+
+	WDT_RESET;
+
 }
 
-void Commutation(void)
-{
-	TO0 = ~TO0;
-	ucDragTmr = 0;
-	if (state_motor >= RUN)
-	{
-		delay_tm0((uiHallPeriod >> 1)-15);
-		//delay1(100);
-	}
-	else
-		delay_tm0(100);	
-		//delay1(100);
 
-			
-	INTEN_HALL= 0;		
-	
-	switch (ucCommStep)
-	{
-	 case 0:	//CT,AB	
-		_pc     = 0b00000010;	
-		_pcps0  = 0x00;
-		_pcps1  = 0b00000010;	
-	    _integ0 = 0b01100000;	//PHASE B DOWN = C3P DOWN
-	 break;
-	
-	 case 1:	//CT,BB
-		_pc     = 0b00001000;
-	    _integ0 = 0b01000100;	//PHASE A UP = C2P UP	
-	 break;
-	 
-	 case 2:	//AT,BB
-		_pcps1  = 0b00000000;
-		_pcps0  = 0b00000010;
-	    _integ0 = 0b01000010;	//PHASE C DOWN = C1P DOWN
-	 break;
-	 
-	 case 3:	//AT,CB
-		_pc     = 0b00100000;
-	    _integ0 = 0b01010000;	//PHASE B UP = C3P UP	
-	 break;
-	 
-	 case 4:	//BT,CB
-		_pcps0  = 0b00100000;
-	    _integ0 = 0b01001000;	//PHASE A DOWN = C2P DOWN
-	 break;
-	 
-	 case 5:	//BT,AB
-		_pc     = 0b00000010;
-	    _integ0 = 0b01000001;	//PHASE C UP = C1P UP	
-	 break;
-	 
-	 default:
-	 	_pc     = 0b00000000;
-	 	_integ0 = 0b01000000;		
-	 break;
-	}
-		
-	if(ucCommStep++ >= 5)
-	{
-		ucCommStep = 0;
-		if(uiCommCycle < 250)
-		    uiCommCycle ++;
-		    
-			if (state_motor == RAMP)
-			{
-				//_hchk_num = 0x10;
-				//if (uiCommCycle > 20)
-				{
-					TO1 = ~TO1;
-					uiDutyRamp += 1;
-					if (uiDutyRamp >= 800)
-					{
-						state_motor = RUN;
-						uiDutyRamp = 800;						
-					}
-					PWM_Duty(uiDutyRamp);					
-					//PWM_Duty(800);
-				}
-			}
-			else if (state_motor == RUN)
-			{
-				_hchk_num = 0x01;
-			}		    
-	}
-		
-	WDT_RESET;
-}
 
-void __attribute ((interrupt(0x08))) ISR_OCP(void)
+void __attribute((interrupt(0x08)))
+ISR_OCP(void)
 {
-	_int_pri2f = 0;
+	_int_pri2f			= 0;
 	WDT_RESET;
-//	bOCPFlag = 1;
+
+	//	bOCPFlag = 1;
 }
 
 
 // 20kHz, 50us
-void __attribute ((interrupt(0x0C))) ISR_PWM0_2(void) // 
+void __attribute((interrupt(0x0C)))
+ISR_PWM0_2(void) // 
 {
-//	uint16 time_delay;
-	
+	//	uint16 time_delay;
+
 	/*
 	bHallU_new = (_mcd & 0x02) == 0x02 ? 1: 0;
 	bHallV_new = (_mcd & 0x04) == 0x04 ? 1: 0;
@@ -243,7 +170,7 @@ void __attribute ((interrupt(0x0C))) ISR_PWM0_2(void) //
 				break;			
 		}
 	}	
-	
+
 	if (hall_bits.byte != 0) // if zc, save current hall time to hall period
 	{
 		TO1 = 1;
@@ -253,10 +180,10 @@ void __attribute ((interrupt(0x0C))) ISR_PWM0_2(void) //
 		hall_bits.byte = 0;		
 		if (uiHallCnt < 1000)
 			uiHallCnt++;		
-	
+
 				
 	}
-	
+
 	// save old comparator value
 	bHallU_old = bHallU_new;
 	bHallV_old = bHallV_new;
@@ -283,7 +210,7 @@ void __attribute ((interrupt(0x0C))) ISR_PWM0_2(void) //
 
 
 	 
-	
+
 	// clear interrupt flags
 
 
@@ -292,10 +219,11 @@ void __attribute ((interrupt(0x0C))) ISR_PWM0_2(void) //
 		ucDragTmr++;
 	*/
 	WDT_RESET;
-	_pwmpf = 0;
-	_pwmd0f = 0;
-	_pwmd1f = 0;
-	_pwmd2f = 0;
-	_int_pri3f = 0;		
+	_pwmpf				= 0;
+	_pwmd0f 			= 0;
+	_pwmd1f 			= 0;
+	_pwmd2f 			= 0;
+	_int_pri3f			= 0;
 }
+
 
--- mydef.h
+++ mydef.h
@@ -2,130 +2,155 @@
 #define _MYDEF_H_
 
 #include "HT66FM5440.h"
+//#define MSKMS_HW
 
-typedef unsigned char boolean; /* Boolean value type. */ 
-typedef unsigned long int uint32; /* Unsigned 32 bit value */ 
-typedef unsigned short uint16; /* Unsigned 16 bit value */ 
-typedef unsigned char uint8; /* Unsigned 8 bit value */ 
-typedef signed long int int32; /* Signed 32 bit value */ 
-typedef signed short int16; /* Signed 16 bit value */ 
+#define PWM_MAX 				800
+#define PWM_DRAG_START			500
+#define PWM_DRAG_END			800
+#define PWM_INC 				5
+
+#define NUM_DRAG				0x16
+#define NUM_RAMP				0x16
+#define NUM_RUN					0x01
+
+typedef unsigned char boolean; /* Boolean value type. */
+typedef unsigned long int uint32; /* Unsigned 32 bit value */
+typedef unsigned short uint16; /* Unsigned 16 bit value */
+typedef unsigned char uint8; /* Unsigned 8 bit value */
+typedef signed long int int32; /* Signed 32 bit value */
+typedef signed short int16; /* Signed 16 bit value */
 typedef signed char int8; /* Signed 8 bit value */
 
-typedef struct{
-	unsigned char	b0 : 1;
-	unsigned char	b1 : 1;
-	unsigned char	b2 : 1;
-	unsigned char	b3 : 1;
-	unsigned char	b4 : 1;
-	unsigned char	b5 : 1;
-	unsigned char	b6 : 1;
-	unsigned char	b7 : 1;
-}_bits;
+typedef struct {
+	unsigned char	b0				: 1;
+	unsigned char	b1				: 1;
+	unsigned char	b2				: 1;
+	unsigned char	b3				: 1;
+	unsigned char	b4				: 1;
+	unsigned char	b5				: 1;
+	unsigned char	b6				: 1;
+	unsigned char	b7				: 1;
+} _bits;
 
-union __hall_type { // define union
-	_bits bits; // using of structure
-	uint8 byte;
+
+union __hall_type { 								// define union
+	_bits			bits;							// using of structure
+	uint8			byte;
 };
 
-#define bNmsFlag status.b0
 
+#define bNmsFlag				status.b0
+
+/*
 #define bHallU_zcr	hall_bits.bits.b0
 #define bHallU_zcf	hall_bits.bits.b1
 #define bHallV_zcr	hall_bits.bits.b2
 #define bHallV_zcf	hall_bits.bits.b3
 #define bHallW_zcr	hall_bits.bits.b4
 #define bHallW_zcf	hall_bits.bits.b5
-//#define bHallU_zc   hall_bits.bits.b6
+#define bHallU_zc	hall_bits.bits.b6
+*/
+// Watch dog timer reset
+#define WDT_RESET				asm("CLR	WDT")
+
+// ADC resolution
+#define ADC_10b 				1
+#define ADC_12b 				0
+
+// analog I/O
+#define AN0 					0
+#define AN1 					1
+#define AN2 					2
+#define AN3 					3
+#define OPA2					4
+#define OPA1					5
+#define OPA0					6
+#define AN6 					7
+#define AN7 					8
+#define ADC_ILIMIT				0
+#define ADC_IS					3
+#define ADC_VSP 				6
+#define ADC_VDC 				7
+
+// motor status
+#define STOP					0
+#define DRAG					1
+#define RAMP					2
+#define RUN 					3
+
+// gate output
+#define UT						_pc0
+#define UB						_pc1
+#define VT						_pc2
+#define VB						_pc3
+#define WT						_pc4
+#define WB						_pc5
+
+// interrupt enable
+#define INTEN_TMB				_int_pri15e
+#define INTEN_HALL				_int_pri1e
+#define INTEN_TM3				_int_pri14e
+#define INTEN_PWM				_int_pri3e
+#define EMI_ON					_emi				= 1
+
+// interrupt flag clear
+#define CLRF_TMB				_int_pri15f = 0; _tbf = 0
+#define CLRF_HALL				_int_pri1f = 0; _halaf = 0; _halbf = 0; _halcf = 0
+#define CLRF_TM3				_int_pri14f = 0; _tm3af = 0
+
+// timer reset
+#define RST_TM0 				_pt0on = 0; _pt0on = 1
+#define RST_TM1 				_pt1on = 0; _pt1on = 1
+#define RST_TM3 				_pt3on = 0; _pt3on = 1
+
+#define START_TM0(x)			_ptm0al = x; _ptm0ah = (x >> 8); _tm0af = 0; _pt0on = 0; _pt0on = 1
+
+#define FLAG_TM0				_tm0af
 
 
-#define WDT_RESET asm("CLR	WDT");
-
-#define ADC_10b 1
-#define ADC_12b	0
-
-#define AN0 0
-#define AN1	1
-#define AN2	2
-#define AN3	3
-#define OPA2 4
-#define OPA1 5
-#define OPA0 6
-#define AN6	7
-#define AN7	8
-
-#define STOP 0
-#define DRAG 1
-#define RAMP 2
-#define RUN 3
+#define PWM_ON					_pwmon				= 1
 
 
-#define LOAD 0
-#define FIRE 1
+// Test pin 
+#define TO0 					_pd2
+#define TO1 					_pd3
 
-#define UT	_pc0
-#define UB	_pc1
-#define VT	_pc2
-#define VB	_pc3
-#define WT	_pc4
-#define WB	_pc5
+#define TB1 					_pb1
+#define TB2 					_pb2
+#define TB0 					_pb0
 
-#define INTEN_TMB	 	_int_pri15e
-#define INTEN_HALL 		_int_pri1e
-#define INTEN_TM3		_int_pri14e
-#define INTEN_PWM		_int_pri3e
+// Function declaration
+void Init_IO(void);
+void Init_System(void);
+void Init_PWM(void);
+void Init_Comp(void);
+void Init_CAPTM(void);
+void Init_TM0(void);
+void Init_TM1(void);
+void Init_TM3(void);
+void Init_Int(void);
+void Init_ADC(void);
+void Init_TimeBase(void);
+void Init_Vars(void);
 
-#define CLRF_TMB	 	_int_pri15f = 0; _tbf = 0
-#define CLRF_HALL 		_int_pri1f = 0; _halaf = 0; _halbf = 0; _halcf = 0
-#define CLRF_TM3		_int_pri14f = 0; _tm3af = 0
+void delay_10u(uint16 x);
+void delay1(uint8 n);
+void delay_ms(uint16 x);
+void delay_tm0(uint16 dly);
+void delay_tm1(uint16 dly);
 
-#define RST_TM0			_pt0on = 0; _pt0on = 1
-#define RST_TM1			_pt1on = 0; _pt1on = 1
-#define RST_TM3			_pt3on = 0; _pt3on = 1
+void ADC_Ch_Sel(uint8 ch);
+void ADC_Trig(void);
+void ADC_AC_CH(uint8 order, uint8 ch);
 
-#define START_TM0(x)	_ptm0al = x; _ptm0ah = (x >> 8); _tm0af = 0; _pt0on = 0; _pt0on = 1
-#define FLAG_TM0		_tm0af
+void PWM_Duty(uint16 duty);
+void PWM_SET(uint8 mode, uint16 duty);
 
-#define PWM_MAX		400
-#define PWM_DRAG 	125
-#define PWM_TU		50
+void TM0_Dly_Set(uint16 dly);
+void TM3_Dly_Set(uint16 dly);
 
-#define TO0 _pd2
-#define TO1 _pd3
+void Hall_Int_Set(uint8 step);
+void Drag_Motor(void);
+void Commutation(void);
 
-#define TB1 _pb1
-#define TB2 _pb2
-#define TB0 _pb0
-
-void 	Init_IO(void);
-void 	Init_System(void);
-void 	Init_PWM(void);
-void 	Init_Comp(void);
-void 	Init_CAPTM(void);
-void 	Init_TM0(void);
-void 	Init_TM1(void);
-void 	Init_TM3(void);
-void 	Init_Int(void);
-void 	Init_ADC(void);
-void 	Init_TimeBase(void);
-void 	Init_Vars(void);
-
-void 	delay_10u(uint16 x);
-void 	delay1(uint8 n);
-void 	delay_ms(uint16 x);
-void 	delay_tm0(uint16 dly);
-void 	delay_tm1(uint16 dly);
-
-void 	ADC_Ch_Sel(uint8 ch);
-void 	ADC_Trig(void);
-void 	ADC_AC_CH(uint8 order, uint8 ch);
-
-void 	PWM_Duty(uint16 duty);
-void 	PWM_SET(uint8 mode, uint16 duty);
-
-void 	TM3_Dly_Set(uint16 dly);
-
-void 	Hall_Int_Set(uint8 step);
-void 	Drag_Motor(void);
-void 	Commutation(void);
-
-#endif 
+#endif
